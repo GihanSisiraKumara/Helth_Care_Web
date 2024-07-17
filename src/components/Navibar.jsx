b25df9056b52cb6e0a747defc91384cc46a328c1
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-07-18 01:19:15 +0800

    home page responsive design

--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-import { Link } from "react-scroll"
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { Link } from "react-scroll";
 
 const Navibar = () => {
+
+    const [menu, setMenu] = useState(false);
+
+    const handleChange = () => {
+        setMenu(!menu);
+    }
+
+    const closeMenu = () => {
+        setMenu(false);
+    }
+
     return (
         <div className='fixed w-full z-10 text-white'>
             <div>
@@ -18,6 +30,30 @@ const Navibar = () => {
                         <Link to='doctors' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Doctors</Link>
                         <Link to='blog' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Blog</Link>
                     </nav>
+                    <div className='hidden lg:flex'>
+                        <button className='bg-yellow-400 text-white px-4 py-2 rounded-md hover:bg-yellow-300 transition duration-300 ease-in-out'>
+                            Contact Us
+                        </button>
+                    </div>
+                    <div className='lg:hidden flex items-center'>
+                        {menu ? (
+                            <AiOutlineClose size={28} onClick={handleChange} />
+                        ) : (
+                            <AiOutlineMenu size={28} onClick={handleChange} />
+                        )}
+                    </div>
+                </div>
+                <div className={'${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-green-400 text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300'}>
+                    <Link to='home' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Home</Link>
+                    <Link to='about' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>About Us</Link>
+                    <Link to='servicess' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Servicess</Link>
+                    <Link to='doctors' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Doctors</Link>
+                    <Link to='blog' spy={true} duration={500} className='hover:text-yellow-400 transition-all cursor-pointer'>Blog</Link>
+                    <div className='lg:hidden'>
+                        <button className='bg-yellow-400 text-white px-4 py-2 rounded-md hover:bg-yellow-300 transition duration-300 ease-in-out'>
+                            Contact Us
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
