722ae9cd049341448683064dca262e8fa5563a95
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-07-18 23:39:01 +0800

    Home page decaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Home from './components/Home'
 import Navibar from './components/Navibar'
 
 const App = () => {
   return (
     <>
       <Navibar />
+      <main>
+        <div id='home'>
+          <Home />
+        </div>
+      </main>
     </>
   )
 }
